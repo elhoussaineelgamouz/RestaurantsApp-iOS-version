955d5b8e13712d8f8fe4fb54bada87bfb2f5173e
elhoussaine elgamouz <elhoussaineelgamouz@gmail.com>
2024-12-08 14:46:04 +0100

    Initial Commit

---
+++ RestaurantsApp/RestaurantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsAppApp.swift
+//  RestaurantsApp
+//
+//  Created by El houssaine El GAMOUZ on 8/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
